80798bb6624032dd862547a9905fef7f24ed885b
shaoyikai <shaoyikai@qq.com>
2016-06-10 10:43:12 +0800

    api update & create use the same _form.php form view

--- views/api/create.php
+++ views/api/create.php
@@ -16,7 +16,8 @@ $this->params['breadcrumbs'][] = $this->title;
 
         <?= $this->render('_form', [
             'model' => $model,
-            'pro_id' => $pro_id
+            'pro_id' => $pro_id,
+            'params' => $params
         ]) ?>
 
     </div>
