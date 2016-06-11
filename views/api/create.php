4ed5c09fe8317b382a9036d4a18905606a15e14a
hanjingjing <shaoyikai@qq.com>
2016-06-11 10:00:00 +0800

    修复面包屑导航

--- views/api/create.php
+++ views/api/create.php
@@ -4,7 +4,7 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
 $this->title = '创建 Api';
-$this->params['breadcrumbs'][] = ['label' => 'Apis', 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => 'Apis', 'url' => ['index', 'pro_id' => $pro_id]];
 $this->params['breadcrumbs'][] = $this->title;
 
 ?>
