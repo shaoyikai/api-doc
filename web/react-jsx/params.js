08e946835e0f4608e3b012cb19d3c1fedf1c1930
shaoyikai <shaoyikai@qq.com>
2016-06-14 23:15:23 +0800

    params.js update

--- web/react-jsx/params.js
+++ web/react-jsx/params.js
@@ -19,12 +19,16 @@ var ParamsThead = React.createClass({
 
 var ParamsTr = React.createClass({
     render: function() {
+        var parm_name = 'name';
+        var parm_type = 'type';
+        var parm_must = '1';
+        var parm_desc = 'description';
         return (
             <tr>
-                <td>45ee4</td>
-                <td>0</td>
-                <td>0</td>
-                <td>4545</td>
+                <td>{parm_name}</td>
+                <td>{parm_type}</td>
+                <td>{parm_must}</td>
+                <td>{parm_desc}</td>
                 <td>
                     <a href="javascript:void(0)" onclick="removeParams(5)" className="btn btn-warning">-</a>
 
@@ -34,6 +38,47 @@ var ParamsTr = React.createClass({
     }
 });
 
+
+/*
+<tr>
+    <td><input type="text" id="par_name"></td>
+    <td>
+        <select id="par_type">
+            <option value="0" selected>string</option>
+            <option value="1">int</option>
+        </select>
+    </td>
+    <td>
+        <select id="par_must">
+            <option value="0" selected>否</option>
+            <option value="1">是</option>
+        </select>
+    </td>
+    <td><input type="text" id="par_desc"></td>
+
+    <td>
+        <?php if($model->isNewRecord):?>
+        <a href="javascript:void(0)" onclick="addParams()" class="btn btn-success">+</a>
+        <?php else:?>
+        <a href="javascript:void(0)" onclick="addParamsOne()" class="btn btn-success">+</a>
+        <?php endif;?>
+    </td>
+
+</tr>
+*/
+var ParamsTrNew = React.createClass({
+    render: function() {
+        return (
+            <tr>
+                <td><Input /></td>
+                <td><Select /></td>
+                <td><Select /></td>
+                <td><Input /></td>
+                <td><Link /></td>
+            </tr>
+        );
+    }
+});
 var ParamsTable = React.createClass({
     render: function() {
         return React.createElement('table',{className:"table table-bordered"},
