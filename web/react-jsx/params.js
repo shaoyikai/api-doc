d6d96305b06ff70bc08ef98a63c33a83bc80620a
shaoyikai <shaoyikai@qq.com>
2016-06-20 23:40:56 +0800

    params.js update 6/20

--- web/react-jsx/params.js
+++ web/react-jsx/params.js
@@ -1,40 +1,7 @@
-var Input = React.createClass({
-    render: function() {
 
-        return (
-            <input type={this.props.type} id={this.props.id} placeholder={this.props.placeholder}/>
-        );
-    }
-});
-var Select1 = React.createClass({
-    render: function() {
-        return (
-            <select id="par_type">
-                <option value="0" selected>string</option>
-                <option value="1">int</option>
-            </select>
-        );
-    }
-});
-var Select2 = React.createClass({
-    render: function() {
-        return (
-            <select id="par_must">
-                <option value="0" selected>否</option>
-                <option value="1">是</option>
-            </select>
-        );
-    }
-});
-var Link = React.createClass({
-    render: function() {
-        return (
-            <a href="javascript:void(0)" onclick="addParamsOne()" className="btn btn-success">+</a>
-        );
-    }
-});
 
 var ParamsTr = React.createClass({
+
     removeParams:function(){
         console.log(this);
     },
@@ -46,7 +13,7 @@ var ParamsTr = React.createClass({
                 <td>{this.props.parm_must}</td>
                 <td>{this.props.parm_desc}</td>
                 <td>
-                    <a href="javascript:void(0)" onClick={this.removeParams} className="btn btn-warning">-</a>
+                    <a href="javascript:void(0)" onClick={this.removeParams.bind(this)} className="btn btn-warning">-</a>
                 </td>
             </tr>
         );
@@ -57,32 +24,53 @@ var ParamsTrNew = React.createClass({
     render: function() {
         return (
             <tr>
-                <td><Input type="text" id="par_id" placeholder="请输入名称"/></td>
-                <td><Select1 /></td>
-                <td><Select2 /></td>
-                <td><Input type="text" id="par_desc" placeholder="请输入描述"/></td>
-                <td><Link /></td>
+                <td><input type="text" id="par_id" placeholder="请输入名称"/></td>
+
+                <td>
+                    <select id="par_type">
+                        <option value="0" selected>string</option>
+                        <option value="1">int</option>
+                    </select>
+                </td>
+
+                <td>
+                    <select id="par_must">
+                        <option value="0" selected>是</option>
+                        <option value="1">否</option>
+                    </select>
+                </td>
+
+                <td><input type="text" id="par_desc" placeholder="请输入描述"/></td>
+
+                <td>
+                    <a href="javascript:void(0)" onclick="addParams()" className="btn btn-success">+</a>
+                </td>
             </tr>
         );
     }
 });
 var ParamsTable = React.createClass({
     render: function() {
-        return React.createElement('table',{className:"table table-bordered"},
-            <tbody>
-                <tr>
-                    <th>参数</th>
-                    <th>类型</th>
-                    <th>是否可选</th>
-                    <th>说明</th>
-                    <th>操作</th>
-                </tr>
-                <ParamsTr parm_name="nameee" parm_type="ddfd" parm_must="1" parm_desc="descriptionddf"/>
-                <ParamsTrNew />
-            </tbody>
+        return (
+            <table className="table table-bordered">
+                <tbody>
+                    <tr>
+                        <th>参数</th>
+                        <th>类型</th>
+                        <th>是否可选</th>
+                        <th>说明</th>
+                        <th>操作</th>
+                    </tr>
+                    <ParamsTr parm_name="nameee" parm_type="ddfd" parm_must="1" parm_desc="descriptionddf"/>
+                    <ParamsTrNew />
+                </tbody>
+            </table>
         );
     }
 });
 
-var box = document.getElementById('params-box');
-ReactDOM.render(<ParamsTable />,box);
+//渲染到页面上
+ReactDOM.render(
+    <ParamsTable />,
+    document.getElementById('params-box')
+);
