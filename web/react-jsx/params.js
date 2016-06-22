4fa60e22274d377529173950a067d463229c9612
shaoyikai <shaoyikai@qq.com>
2016-06-22 22:31:43 +0800

    params.js update 6/22

--- web/react-jsx/params.js
+++ web/react-jsx/params.js
@@ -2,18 +2,20 @@
 
 var ParamsTr = React.createClass({
 
-    removeParams:function(){
-        console.log(this);
+    removeParams:function(event){
+        alert('remove self');
     },
     render: function() {
+        var data = this.props.data;
+
         return (
             <tr>
-                <td>{this.props.parm_name}</td>
-                <td>{this.props.parm_type}</td>
-                <td>{this.props.parm_must}</td>
-                <td>{this.props.parm_desc}</td>
+                <td>{data.parm_name}</td>
+                <td>{data.parm_type}</td>
+                <td>{data.parm_must}</td>
+                <td>{data.parm_desc}</td>
                 <td>
-                    <a href="javascript:void(0)" onClick={this.removeParams.bind(this)} className="btn btn-warning">-</a>
+                    <a href="javascript:void(0)" onClick={this.removeParams} className="btn btn-warning">-</a>
                 </td>
             </tr>
         );
@@ -21,10 +23,35 @@ var ParamsTr = React.createClass({
 });
 
 var ParamsTrNew = React.createClass({
+    getInitialState:function(){
+        return {
+            value1:'hello!',
+            value2:'hello!'
+        };
+    },
+    handleChange:function(event){
+        this.setState({
+            value1:event.target.value,
+            value2:event.target.value
+        });
+    },
+
+    addParams:function(){
+        alert('heheheheh');
+    },
+
     render: function() {
         return (
             <tr>
-                <td><input type="text" id="par_id" placeholder="请输入名称"/></td>
+                <td>
+                    <input
+                        type="text"
+                        id="par_id"
+                        placeholder="请输入名称"
+                        value={this.state.value1}
+                        onChange={this.handleChange}
+                    />
+                </td>
 
                 <td>
                     <select id="par_type">
@@ -40,10 +67,18 @@ var ParamsTrNew = React.createClass({
                     </select>
                 </td>
 
-                <td><input type="text" id="par_desc" placeholder="请输入描述"/></td>
+                <td>
+                    <input
+                        type="text"
+                        id="par_desc"
+                        placeholder="请输入描述"
+                        value={this.state.value2}
+                        onChange={this.handleChange}
+                    />
+                </td>
 
                 <td>
-                    <a href="javascript:void(0)" onclick="addParams()" className="btn btn-success">+</a>
+                    <a href="javascript:void(0)" onClick={this.addParams} className="btn btn-success">+</a>
                 </td>
             </tr>
         );
@@ -51,6 +86,12 @@ var ParamsTrNew = React.createClass({
 });
 var ParamsTable = React.createClass({
     render: function() {
+        var data = {
+            parm_name:"nameee",
+            parm_type:"string",
+            parm_must:"1",
+            parm_desc:"desssc"
+        };
         return (
             <table className="table table-bordered">
                 <tbody>
@@ -61,7 +102,7 @@ var ParamsTable = React.createClass({
                         <th>说明</th>
                         <th>操作</th>
                     </tr>
-                    <ParamsTr parm_name="nameee" parm_type="ddfd" parm_must="1" parm_desc="descriptionddf"/>
+                    <ParamsTr data={data}/>
                     <ParamsTrNew />
                 </tbody>
             </table>
